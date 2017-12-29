b08cbcd1f9c84d40853bfb8d04ead1f8fc0f7ec0
mihaiborbea <borbeamihai@hotmail.com>
2017-12-30 01:22:04 +0200

    mangoose added

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ var users = require('./routes/users');
 
 var app = express();
 
+var bluebird = require('bluebird');
+var mongoose = require('mongoose');
+mongoose.Promise = bluebird
+
+// db connection
+mongoose.connect('mongodb://127.0.0.1:27017/todoapp', { useMongoClient: true})
+.then(()=> { console.log(`Succesfully Connected to the Mongodb Database  at URL : mongodb://127.0.0.1:27017/todoapp`)})
+.catch(()=> { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/todoapp`)})
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
