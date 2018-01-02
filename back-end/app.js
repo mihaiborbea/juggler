dd10f62e6e855015029697e993c943171c6934a4
mihaiborbea <borbeamihai@hotmail.com>
2018-01-02 15:22:20 +0200

    GH-1 : implemented todo model

--- back-end/app.js
+++ back-end/app.js
@@ -33,10 +33,7 @@ mongoose
 // CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
