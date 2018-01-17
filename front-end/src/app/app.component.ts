288c572e3804a66cc2dede0c44a992769094bf12
mihaiborbea <borbeamihai@hotmail.com>
2018-01-17 22:22:46 +0200

    first version of the front-end app

---
+++ front-end/src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './services/todo.service';
+import { TodoModel } from './models/todo.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  constructor(private todoService: TodoService) {
+    //
+  }
+
+  public newTodo: TodoModel = new TodoModel();
+
+  todosList: TodoModel[];
+  editTodos: TodoModel[] = [];
+
+  ngOnInit(): void {
+    this.todoService.getTodos()
+      .subscribe((todos) => {
+        this.todosList = todos;
+        console.log(todos);
+      })
+  }
+
+  create() {
+    this.todoService.createTodo(this.newTodo)
+      .subscribe((res) => {
+        this.todosList.push(res.data)
+        this.newTodo = new TodoModel()
+      })
+  }
+
+  editTodo(todo: TodoModel) {
+    console.log(todo)
+    if (this.todosList.includes(todo)) {
+      if (!this.editTodos.includes(todo)) {
+        this.editTodos.push(todo);
+      } else {
+        this.editTodos.splice(this.editTodos.indexOf(todo), 1);
+        this.todoService.editTodo(todo).subscribe(res => {
+          console.log('Update Succesful')
+        }, err => {
+          this.editTodo(todo);
+          console.error('Update Unsuccesful')
+        })
+      }
+    }
+  }
+
+  doneTodo(todo: TodoModel) {
+    todo.status = 'Done'
+    this.todoService.editTodo(todo).subscribe(res => {
+      console.log('Update Succesful')
+    }, err => {
+      this.editTodo(todo);
+      console.error('Update Unsuccesful')
+    })
+  }
+
+  submitTodo(event, todo: TodoModel) {
+    if (event.keyCode == 13) {
+      this.editTodo(todo);
+    }
+  }
+
+  deleteTodo(todo: TodoModel) {
+    this.todoService.deleteTodo(todo._id).subscribe(res => {
+      this.todosList.splice(this.todosList.indexOf(todo), 1);
+    })
+  }
+}
