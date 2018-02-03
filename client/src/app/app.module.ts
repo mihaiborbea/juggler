8e0ef1002388e50ff52111b73dbfad6641c74a6d
mihaiborbea <borbeamihai@hotmail.com>
2018-02-03 18:18:54 +0200

    started updating the client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
